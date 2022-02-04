6ca8455d5f1aaf19892ab0d669ca6531e3e951c2
CamilleAbella <camille.abella@hotmail.fr>
2022-02-04 17:49:18 +0100

    fix characters

--- src/graphics.ts
+++ src/graphics.ts
@@ -241,10 +241,7 @@ export class Graphics extends extension.ExtendedCompositeEntity {
       );
 
       const speakerLC = speaker.toLowerCase();
-      if (
-        (autoShow && speakerLC !== "you") ||
-        (speakerLC === this._lastCharacter && mood !== this._lastMood)
-      ) {
+      if (autoShow && speakerLC !== "you") {
         this.addCharacter(speakerLC, mood?.toLowerCase());
       }
     } else {
