454511e39211dd5169c0cd346267d2ebcbe8e895
Jesse Himmelstein <jesse@heavenstone.net>
2022-03-30 13:57:10 +0200

    Extra event checking on full screen change

--- src/game.ts
+++ src/game.ts
@@ -201,3 +201,4 @@ booyah.go({
 // Resize now, and force the resize to happen when the window size changes
 resizeHtmlLayer(screenSize);
 window.addEventListener("resize", () => resizeHtmlLayer(screenSize));
+document.addEventListener("fullscreenchange", () => resizeHtmlLayer(screenSize));
